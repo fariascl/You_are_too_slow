d6c66a01cbb0b4a3e83fce9279076a43794389bd
PabloIMG <pablo16mg@gmail.com>
2021-12-12 17:13:06 -0300

    Creacion carpeta para pruebas

---
+++ pruebas/pruebaServer.c
@@ -0,0 +1,20 @@
+/*
+    Código destinado a realizar pruebas con el servidor.
+    Historial de pruebas:
+
+        1) Manejar programa con solo 1 hijo []
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+
+int main()
+{
+    
+
+    return  0;
+}
